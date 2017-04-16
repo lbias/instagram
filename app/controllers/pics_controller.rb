cdff118cfc073cdec366fa6fb9a39ffdebf04c6c
sam <morvazhu@gmail.com>
2017-04-16 12:13:16 +0800

    install simple_form, create new pics and view file

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,22 @@
 class PicsController < ApplicationController
   def index
   end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+
 end
